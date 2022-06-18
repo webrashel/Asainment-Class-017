b2511154e2d2b88fbcf4949f64b10713c8b1a4ee
webrashel <rashelrana931@gmail.com>
2022-06-18 03:20:21 -0700

    rashel

--- Admin Panel/Banner.php
+++ Admin Panel/Banner.php
@@ -1,34 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>themelock.com - Limitless - Responsive Web Application Kit by Eugene Kopyov</title>
-
-	<!-- Global stylesheets -->
-	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-	<link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/minified/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/minified/core.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/minified/components.min.css" rel="stylesheet" type="text/css">
-	<link href="assets/css/minified/colors.min.css" rel="stylesheet" type="text/css">
-	<!-- /global stylesheets -->
-
-	<!-- Core JS files -->
-	<script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script>
-	<script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
-	<script type="text/javascript" src="assets/js/core/libraries/bootstrap.min.js"></script>
-	<script type="text/javascript" src="assets/js/plugins/loaders/blockui.min.js"></script>
-	<!-- /core JS files -->
-
-	<!-- Theme JS files -->
-	<script type="text/javascript" src="assets/js/plugins/tables/datatables/datatables.min.js"></script>
-	<script type="text/javascript" src="assets/js/plugins/forms/selects/select2.min.js"></script>
-
-	<script type="text/javascript" src="assets/js/core/app.js"></script>
-	<script type="text/javascript" src="assets/js/pages/datatables_basic.js"></script>
-	<!-- /theme JS files -->
+	<?php  ?>
 
 </head>
 
@@ -2476,6 +2449,7 @@
 
 	</div>
 	<!-- /page container -->
+	<?php include "includs/script.php"; ?>
 
 </body>
 </html>
---
+++ Admin Panel/includs/head.php
@@ -0,0 +1,25 @@
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Admin Panel</title>
+
+	<!-- Global stylesheets -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+	<link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/minified/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/minified/core.min.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/minified/components.min.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/minified/colors.min.css" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<Style>
+
+		.panel-flat>.panel-heading{
+
+
+			border-bottom: 1px solid #bbbbbb;
+		}
+	</Style>
+
+</head>
